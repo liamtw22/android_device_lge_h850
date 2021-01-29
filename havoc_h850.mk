7a2784fe178bc6ed5506398fd264a79e10a20812
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2021-01-29 08:02:23 -0500

    Update and rename lineage_h850.mk to havoc_h850.mk

--- havoc_h850.mk
+++ havoc_h850.mk
@@ -19,14 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit from h815 device
 $(call inherit-product, device/lge/h850/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h850
-PRODUCT_NAME := lineage_h850
+PRODUCT_NAME := havoc_h850
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H850
 PRODUCT_MANUFACTURER := LGE
