1614a9fb8f2f0dca8bb0d49226d26e70359d5c68
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2021-02-23 16:45:30 -0500

    Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_h850.mk
+    $(LOCAL_DIR)/havoc_h850.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_h850-userdebug \
-    lineage_h850-eng
+    havoc_h850-userdebug \
+    havoc_h850-eng
